9ed26e1f63902723d3b127175654d7160563374d
Dmitry Birin <dmitry.birin@gmail.com>
2017-08-20 18:47:41 +0300

    start-maximized for all systems added

--- driver_wrapper.py
+++ driver_wrapper.py
@@ -3,22 +3,21 @@ from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 
 class DriverWrapper():
-	def __init__(self, browser_name: str):
-		try:
-			if browser_name.lower() == 'chrome':
-				self.chrome_options = webdriver.ChromeOptions()
-				# self.browser_options.add_argument("--incognito")
-				# self.browser_options.add_argument("--start-maximized")
-				self.driver = webdriver.Chrome(chrome_options=self.chrome_options)
-			if browser_name.lower() == 'firefox':
-				self.firefox_options = webdriver.FirefoxProfile()
-				self.driver = webdriver.Firefox(firefox_profile=self.firefox_options)
-		except WebDriverException as e:
-			print('Cannot get the driver for the {}.\n{}'.format(browser_name.capitalize(), e))
-			sys.exit(1)
-			
-	
-	def go_to(self, url: str, return_page=None):
-		self.driver.get(url)
-		if return_page is not None:
-			return return_page(self)
+    def __init__(self, browser_name: str):
+        try:
+            if browser_name.lower() == 'chrome':
+                self.chrome_options = webdriver.ChromeOptions()
+                self.chrome_options.add_argument('--kiosk')
+                self.chrome_options.add_argument('--start-maximized')
+                self.driver = webdriver.Chrome(chrome_options=self.chrome_options)
+            else:
+                print('{} is not supported'.format(browser_name))
+                sys.exit(1)    
+        except WebDriverException as e:
+            print('Cannot get the driver for the {}.\n{}'.format(browser_name.capitalize(), e))
+            sys.exit(1)
+
+    def go_to(self, url: str, return_page=None):
+        self.driver.get(url)
+        if return_page is not None:
+            return return_page(self)
