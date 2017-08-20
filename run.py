13f05646444596b3390f3f0b02425c0d74db0f19
Dmitry Birin <dmitry.birin@gmail.com>
2017-08-20 19:05:47 +0300

    run refactored

--- run.py
+++ run.py
@@ -1,13 +1,8 @@
 from driver_wrapper import DriverWrapper
 from page_objects.home import Home
 
-yandex_url = 'https://yandex.ru'
-
-# 'https://github.com/mozilla/geckodriver/releases'
-# 'https://chromedriver.storage.googleapis.com/index.html?path=2.31/'
-
 filteredResults = DriverWrapper('chrome') \
-.go_to(yandex_url, Home) \
+.go_to('https://yandex.ru', Home) \
 .go_to_market() \
 .choose_main_menu_item('Электроника') \
 .click_on_submenu_item('Мобильные телефоны') \
